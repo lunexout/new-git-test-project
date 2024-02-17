84c1505b685dff66bf8ba6409ad4987bb1d78725
jano <lunexout@gmail.com>
2024-02-17 15:53:01 +0400

    add profile page

---
+++ src/pages/common.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Profile } from "./components/profile";
+
+const Page = () => {
+  return (
+    <>
+      <Profile />
+    </>
+  );
+};
+
+export default Page;
