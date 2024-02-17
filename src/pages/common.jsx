75426e4d3050a5f6f2eb08291a9ee9c3cfc6cfeb
Jano Gazashvili <69036038+lunexout@users.noreply.github.com>
2024-02-17 16:17:05 +0400

    add settings page (#2)

--- src/pages/common.jsx
+++ src/pages/common.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Profile } from "./components/profile";
+import { Settings } from "./components/settings";
 
 const Page = () => {
   return (
     <>
       <h1>hELLOS1</h1>
       <h1>hELLOS</h1>
+      <Settings />
       <Profile />
     </>
   );
