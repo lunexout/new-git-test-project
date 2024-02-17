f38129603c796195311f3abf22a46350bc5d6399
jano <lunexout@gmail.com>
2024-02-17 15:58:53 +0400

    add settings page

---
+++ src/pages/common.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Settings } from "./components/settings";
+
+const Page = () => {
+  return (
+    <>
+      <h1>hELLOS1</h1>
+      <h1>hELLOS</h1>
+      <Settings />
+    </>
+  );
+};
+
+export default Page;
