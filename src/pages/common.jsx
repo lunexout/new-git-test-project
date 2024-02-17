f4da1d111c8f4cf345c581d9a444802356692700
Jano Gazashvili <69036038+lunexout@users.noreply.github.com>
2024-02-17 16:19:02 +0400

    User common page (#3)
    * add profile page (#1)
    
    * add profile page
    
    * fix comments
    
    * fix comments
    
    * add settings page (#2)

---
+++ src/pages/common.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Profile } from "./components/profile";
+import { Settings } from "./components/settings";
+
+const Page = () => {
+  return (
+    <>
+      <h1>hELLOS1</h1>
+      <h1>hELLOS</h1>
+      <Settings />
+      <Profile />
+    </>
+  );
+};
+
+export default Page;
