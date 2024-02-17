3a68fd3edfdd7e4b0bed883fa5f8ea2d651f0f32
Jano Gazashvili <69036038+lunexout@users.noreply.github.com>
2024-02-17 16:01:30 +0400

    add profile page (#1)
    * add profile page
    
    * fix comments
    
    * fix comments

---
+++ src/pages/common.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Profile } from "./components/profile";
+
+const Page = () => {
+  return (
+    <>
+      <h1>hELLOS1</h1>
+      <h1>hELLOS</h1>
+      <Profile />
+    </>
+  );
+};
+
+export default Page;
